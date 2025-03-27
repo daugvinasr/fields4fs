ebca1fb368f68fd2d5f23e905a4a8760592db86c
daugvinasr <daugvinasr@gmail.com>
2025-03-27 18:17:18 +0200

    migrate to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  output: "static",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: 'https://fields4fs.zod.nu',
+  integrations: [sitemap()],
 });
